2162e0491b378d0e127fe2745e0be5301b39b682
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2020-05-20 20:51:37 -0600

    LAB 8 terminado

---
+++ CottoJavier_LAB8/Assets/Scripts/Lampara.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lampara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip hola;
+    public AudioSource audioS;
+    public Light light;
+    void Start()
+    {
+
+        audioS = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        light.intensity = Mathf.Sin(Time.time *10) * 100;
+
+    }
+
+    private void OnTriggerEnter()
+    {
+        light.enabled = true;
+        audioS.PlayOneShot(hola);
+    }
+
+    private void OnTriggerExit()
+    {
+        light.enabled = false;
+        audioS.PlayOneShot(hola);
+    }
+}
---
+++ CottoJavier_LAB8/Assets/Scripts/Linterna.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Linterna : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Light light;
+    public AudioClip OnOff;
+    public AudioSource audioS;
+
+    void Start()
+    {
+
+        light = GetComponent<Light>();
+
+        audioS = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (light.intensity > 0)
+            {
+                light.intensity = 0;
+                audioS.PlayOneShot(OnOff);
+            }
+            else
+            {
+                light.intensity = 8;
+                audioS.PlayOneShot(OnOff);
+            }
+        }
+
+        /*RaycastHit hitInfo;
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(myRay, out hitInfo, 20))
+                Debug.Log(hitInfo.collider.gameObject.name);*/
+
+
+    }
+}
